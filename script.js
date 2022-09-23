c65319596c02739d59ec9f8cdac5ac0653f88e29
Douglasso10 <84336458+Douglasso10@users.noreply.github.com>
2022-09-22 21:18:24 -0300

    testando a vitoria no jogo

--- script.js
+++ script.js
@@ -3,12 +3,31 @@ const conteiner = document.querySelector('[data-conteiner]');
 
 let vezDoCirculo;
 
+const combinacaoDeVitoria = [
+    [0, 1, 2],
+    [3, 4 ,5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
 const comecarJogo = () => {
     for (const linha of linhasElementos){
         linha.addEventListener('click', clicar, {once:true});
     }
     vezDoCirculo = false;
     conteiner.classList.add('x');
+}
+
+const checandoVitoria = (jogadorAtual) => {
+    combinacaoDeVitoria.some(combinacao => {
+        return combinacao.every(index => {
+            return conteiner[index].classList.conteins(jogadorAtual);
+        })
+    })
 }
 
 const marcando = (linha, adicionar) =>{
@@ -34,6 +53,12 @@ const clicar = (e) => {
     const adicionar = vezDoCirculo ? "circulo" : "x";
     
     marcando(linha,adicionar);
+
+    // Verificação de vitória
+    const vitoria = checandoVitoria();
+    if(vitoria){
+        console.log("ganhei!!")
+    }
 
     //mudar o simbolo
 
